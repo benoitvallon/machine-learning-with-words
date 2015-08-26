f56e2d1a51de2306db264e22f5d2d3407a10ee24
Benoit VALLON <benoitvallon@gmail.com>
2015-08-26 18:10:27 -0400

    Adds the neural network

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var mimir = require('mimir'),
+  brain = require('brain'),
   bow = mimir.bow,
   dict = mimir.dict;
 
@@ -11,3 +12,82 @@ voc = dict(texts);
 console.log('voc', voc);
 console.log(bow("John likes to watch movies. Mary likes movies too.", voc));
 console.log(bow("John also likes to watch football games.", voc));
+
+texts = [
+  // history
+  "The end of the Viking-era in Norway is marked by the Battle of Stiklestad in 1030",
+  "The end of the Viking Age is traditionally marked in England by the failed invasion attempted by the Norwegian king Harald III ",
+  "The earliest date given for a Viking raid is 787 AD when, according to the Anglo-Saxon Chronicle, a group of men from Norway sailed to the Isle of Portland in Dorset",
+  // programming
+  "A programming language is a formal constructed language designed to communicate instructions to a machine, particularly a computer. Programming languages can be used to create programs to control the behavior of a machine or to express algorithms.",
+  "Thousands of different programming languages have been created, mainly in the computer field, and many more still are being created every year.",
+  "The description of a programming language is usually split into the two components of syntax (form) and semantics (meaning). Some languages are defined by a specification document (for example, the C programming language is specified by an ISO Standard), while other languages (such as Perl) have a dominant implementation that is treated as a reference",
+
+  // music
+  "Classical music is art music produced or rooted in the traditions of Western music (both liturgical and secular)",
+  "European music is largely distinguished from many other non-European and popular musical forms by its system of staff notation, in use since about the 16th century",
+  "classical music has been noted for its development of highly sophisticated forms of instrumental music."
+]
+
+function vec_result(res, num_classes) {
+  var i = 0,
+  vec = [];
+  for (i; i < num_classes; i += 1) {
+   vec.push(0);
+  }
+  vec[res] = 1;
+  return vec;
+}
+function maxarg(array) {
+  return array.indexOf(Math.max.apply(Math, array));
+}
+
+var ANN_Classes = {
+    HISTORY: 0,
+    PROGRAMMING: 1,
+    MUSIC: 2
+  },
+  classes_array = Object.keys(ANN_Classes),
+  dict = mimir.dict(texts),
+  traindata = [
+    [mimir.bow(texts[0], dict), ANN_Classes.HISTORY],
+    [mimir.bow(texts[1], dict), ANN_Classes.HISTORY],
+    [mimir.bow(texts[2], dict), ANN_Classes.HISTORY],
+    [mimir.bow(texts[3], dict), ANN_Classes.PROGRAMMING],
+    [mimir.bow(texts[4], dict), ANN_Classes.PROGRAMMING],
+    [mimir.bow(texts[5], dict), ANN_Classes.PROGRAMMING],
+    [mimir.bow(texts[6], dict), ANN_Classes.MUSIC],
+    [mimir.bow(texts[7], dict), ANN_Classes.MUSIC],
+    [mimir.bow(texts[8], dict), ANN_Classes.MUSIC]
+  ],
+  test_history = 'The beginning of the Viking Age in the British Isles is, however, often given as 793.',
+  test_music = 'Baroque music is a style of Western art music composed from approximately 1600 to 1750',
+  test_programming = 'The library brings different computer vision algorithms and techniques into the browser environment.'
+  test_bow_history = mimir.bow(test_history, dict),
+  test_bow_music = mimir.bow(test_music, dict);
+  test_bow_programming = mimir.bow(test_programming, dict);
+
+
+var net = new brain.NeuralNetwork(),
+  ann_train = traindata.map(function (pair) {
+    return {
+      input: pair[0],
+      output: vec_result(pair[1], 3)
+    };
+  });
+net.train(ann_train);
+
+var predict = net.run(test_bow_history);
+
+console.log(predict);
+console.log(classes_array[maxarg(predict)]);
+
+var predict = net.run(test_bow_music);
+
+console.log(predict);
+console.log(classes_array[maxarg(predict)]);
+
+var predict = net.run(test_bow_programming);
+
+console.log(predict);
+console.log(classes_array[maxarg(predict)]);
